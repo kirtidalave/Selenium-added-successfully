4af2e59b8e209d16975b9b53d47dd621c45aae79
kirtidalave <dalavekirti@gmail.com>
2022-03-30 16:45:30 +0530

    facebook added successfully

--- test_facebooklogin.py
+++ test_facebooklogin.py
@@ -1,15 +1,17 @@
 import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
 driver=webdriver.Chrome(ChromeDriverManager().install())
+username=input("Enter Username")
+password=input("Enter password")
 print("Test case Started")
 driver.maximize_window()
-driver.get("https://www.facebook.com")
+driver.get("https://www.facebook.com/")
 time.sleep(60)
-driver.find_elements_by_name("username").send_keys("")
+driver.find_elements_by_name("email").send_keys("username")
 driver.sleep(5)
-driver.find_element_by_id("pass").send_keys("")
+driver.find_element_by_id("pass").send_keys("password")
 time.sleep(1)
 driver.find_element_by_name("login").click()
 time.sleep(2)
