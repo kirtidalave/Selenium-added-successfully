2bf742aa82c78c4f13d0522c54bc39523a790d82
kirtidalave <dalavekirti@gmail.com>
2022-03-30 17:20:45 +0530

    username added

--- test_instagramlogin.py
+++ test_instagramlogin.py
@@ -1,16 +1,19 @@
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+user_input = input("please Enter your user name\n")
+user_input2 = input("please Enter your password\n")
 
 
 driver=webdriver.Chrome(ChromeDriverManager().install())
+
 print("Test case Started")
 driver.maximize_window()
 driver.get("https://www.instagram.com")
 time.sleep(60)
-driver.find_elements_by_name("username").send_keys("kirti_____20")
-driver.sleep(5)
-driver.find_element_by_id("pass").send_keys("Shivanid@123")
+driver.find_elements_by_name("username").send_keys("user_input")
+driver.sleep(1)
+driver.find_element_by_id("pass").send_keys("user_input2")
 time.sleep(1)
 driver.find_element_by_name("login").click()
 time.sleep(2)
